a565fc33a1cf7866086a4a50bd00f68f83adebf0
itversity <dgadiraju@itversity.com>
2020-08-31 17:05:45 +0530

    Docker Compose and Postgres related changes

--- ch04-docker-compose/wait-for-postgres.sh
+++ ch04-docker-compose/wait-for-postgres.sh
@@ -7,10 +7,28 @@ host="$1"
 shift
 cmd="$@"
 
-until PGPASSWORD=$POSTGRES_PASSWORD dpsql -h "$host" -U "postgres" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
+
+PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" << EOF
+CREATE DATABASE sms_db;
+CREATE USER sms_user WITH ENCRYPTED PASSWORD 'itversity';
+GRANT ALL PRIVILEGES ON DATABASE sms_db TO sms_user;
+
+\c sms_db
+
+CREATE TABLE hw (
+    i INT,
+    s VARCHAR(30)
+);
+
+INSERT INTO hw VALUES (1, 'Hello');
+INSERT INTO hw VALUES (2, 'World');
+
+COMMIT;
+EOF
 
 >&2 echo "Postgres is up - executing command"
 exec $cmd
