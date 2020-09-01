329157fb576de1001762e6aa7d479e33c4359c13
Sai Teja Rayala <saiteja@itversity.in>
2020-09-01 14:32:48 +0530

    validated DB for ch04

--- ch04_docker_compose/app.py
+++ ch04_docker_compose/app.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sms_user:itversity@db:5432/sms_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-
-@app.route('/')
-def index():
-    return 'Hello World, Getting Started'
+from .routes import *
--- ch04_docker_compose/wait-for-postgres.sh
+++ ch04_docker_compose/wait-for-postgres.sh
@@ -19,15 +19,17 @@ GRANT ALL PRIVILEGES ON DATABASE sms_db TO sms_user;
 
 \c sms_db
 
-CREATE TABLE hw (
-    i INT,
+CREATE TABLE IF NOT EXISTS hw (
+    i INT PRIMARY KEY,
     s VARCHAR(30)
 );
 
-INSERT INTO hw VALUES (1, 'Hello');
-INSERT INTO hw VALUES (2, 'World');
+INSERT INTO hw VALUES (1, 'Hello') ON CONFLICT DO NOTHING;
+INSERT INTO hw VALUES (2, 'World') ON CONFLICT DO NOTHING;
+
 
 GRANT ALL PRIVILEGES ON DATABASE sms_db TO sms_user;
+GRANT ALL ON TABLE hw to PUBLIC;
 EOF
 
 >&2 echo "Postgres is up - executing command"
